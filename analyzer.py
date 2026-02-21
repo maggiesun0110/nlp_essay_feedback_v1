afae6b94e8e68efce7213cfaa09a6ee8df8eeb10
Maggie <maggiesun625@gmail.com>
2026-02-21 15:52:13 -0800

    basic n2sswsw

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,7 @@
+#TODO
+#next step = morphlogy based detection (word form)
 import spacy
+from collections import defaultdict
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -15,6 +18,19 @@ def detect_passive(text):
 
     return passive_sentences
 
+def no_two_sentences_start_with_same_word(text):
+    doc = nlp(text)
+    starter_dict = defaultdict(list)
+
+    #each sentence is a span object so we can iterate through doc.sents
+    for sent in doc.sents:
+        first_word = sent[0].text.lower()
+        starter_dict[first_word].append(sent.text)
+
+    return starter_dict
+        
+
 if __name__ == "__main__":
-    text = "the ball was thrown by Maggie. Maggie caught it. Is this passive? Passive was done by this."
+    text = "the ball was thrown by Maggie. Maggie caught it. Is this passive? Passive was done by this. Passive voice is bad. This is a sentence. This is another sentence."
     print(detect_passive(text))
+    print(no_two_sentences_start_with_same_word(text))
